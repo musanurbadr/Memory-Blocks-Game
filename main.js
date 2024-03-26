2bd828b3c3a8f61c0056432c7bd7e4a6ba320475
musanurbadr <111696681+musanurbadr@users.noreply.github.com>
2024-03-26 22:30:57 +0300

    Memory Blocks Game

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+
+document.querySelector(".control-buttons span").onclick = function(){
+    
+    let yourName = prompt("What is you name ");
+
+    if (yourName == null || yourName == ""){
+        document.querySelectorAll(".name span").innerHTML = 'Unknow';
+    }else{
+        document.querySelector(".name span").innerHTML = yourName;
+    }
+    document.querySelector(".control-buttons").remove();
+}; 
+
+let duration = 1000; 
+
+let blockscontainer = document.querySelector("..memory-game-block");
+
+let blocks = Array.from(blockscontainer.children);
+console.log(blocks);
